ad03fb86b42b91ca0d78e124287d037517ad9106
NicolaBellini <Bellininicola4366@gmail.com>
2024-11-20 17:05:38 +0100

    add gsap effect and hero video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,54 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { heroVideo, smallHeroVideo } from "../utils";
+import { useState } from "react";
+import { useEffect } from "react";
+
 const Hero = () => {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      duration: 5,
+      delay: 1.5,
+    });
+  }, []);
+
   return (
     <section className="w-full nav-height bg-black">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p className="hero-title">IPhone 15 Pro</p>
+        <p id="hero" className="hero-title">
+          IPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+            className="pointer-events-none"
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
       </div>
     </section>
   );
