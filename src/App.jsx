0e762f9097534b97eba4829fe9a83dcdf0e30d91
NicolaBellini <Bellininicola4366@gmail.com>
2024-11-20 16:47:41 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/navbar";
+import Hero from "./components/Hero";
+import HighLights from "./components/HighLights";
+
+function App() {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <HighLights />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+const Hero = () => {
+  return (
+    <section className="w-full nav-height bg-black">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p className="hero-title">IPhone 15 Pro</p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
